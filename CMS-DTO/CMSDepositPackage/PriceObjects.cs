5d9010d4ed5ae955db7b13ac2dc5e1e84e316289
David <voquoctuanqn@gmail.com>
2019-03-20 23:00:38 +0700

    deposit

---
+++ CMS-DTO/CMSDepositPackage/PriceObjects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS_DTO.CMSDepositPackage
+{
+    public class PriceObjects
+    {
+        public string symbol { get; set; }
+        public decimal? priceChange { get; set; }
+        public decimal? priceChangePercent { get; set; }
+        public decimal? weightedAvgPrice { get; set; }
+        public decimal? prevClosePrice { get; set; }
+        public decimal? lastPrice { get; set; }
+    }
+}
--- CMS-Web/Controllers/DepositPackageController.cs
+++ CMS-Web/Controllers/DepositPackageController.cs
@@ -1,9 +1,14 @@
 ﻿using CMS_DTO;
+using CMS_DTO.CMSDepositPackage;
 using CMS_Shared.CMSEmployees;
 using CMS_Web.Web.App_Start;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -31,6 +36,25 @@ namespace CMS_Web.Controllers
         public ActionResult AddDepositPackage(CMS_DepositPackageModel item)
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetPrice()
+        {
+            var Priceobj = new PriceObjects();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+                HttpResponseMessage response = await client.GetAsync("https://api.binance.com/api/v1/ticker/24hr?symbol=XRPUSDT");
+                if (response.IsSuccessStatusCode)
+                {
+                    var res = await response.Content.ReadAsStringAsync();
+                    Priceobj = JsonConvert.DeserializeObject<PriceObjects>(res);
+                }
+            }
+            return Json(Priceobj, JsonRequestBehavior.AllowGet);
         }
     }
 }
