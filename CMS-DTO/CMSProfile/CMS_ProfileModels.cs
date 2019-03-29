f789ff4e4e23fb9680cce5b6c5b010615d1ee0e5
David <voquoctuanqn@gmail.com>
2019-03-29 22:35:08 +0700

    profile

---
+++ CMS-DTO/CMSProfile/CMS_ProfileModels.cs
@@ -0,0 +1,21 @@
+﻿using CMS_DTO.CMSMarketing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS_DTO.CMSProfile
+{
+    public class CMS_ProfileModels
+    {
+        public List<CMS_MarketingModels> OTPs { get; set; }
+        public List<CMS_MarketingModels> Marketings { get; set; }
+
+        public CMS_ProfileModels()
+        {
+            OTPs = new List<CMS_MarketingModels>();
+            Marketings = new List<CMS_MarketingModels>();
+        }
+    }
+}
--- CMS-Web/Controllers/ProfileController.cs
+++ CMS-Web/Controllers/ProfileController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CMS_DTO.CMSProfile;
+using CMS_Shared;
+using CMS_Shared.CMSMarketing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,18 @@ namespace CMS_Web.Controllers
 {
     public class ProfileController : Controller
     {
+        private CMSMarketingFactory _factory;
+        public ProfileController()
+        {
+            _factory = new CMSMarketingFactory();
+        }
         // GET: Profile
         public ActionResult Index()
         {
-            return View();
+            var model = new CMS_ProfileModels();
+            model.OTPs = _factory.GetList((int)Commons.SMSType.OTP);
+            model.Marketings = _factory.GetList((int)Commons.SMSType.Marketing);
+            return View(model);
         }
     }
 }
