304cf1af334e0f1402132b7d2d39317c81e5f812
DESKTOP-GRKQD38\ThanhPham <thanh.pham@dki-sd.com>
2019-04-01 10:03:28 +0700

    Update code centri (update status sim)

--- CMS-Web/Controllers/CentrifugeController.cs
+++ CMS-Web/Controllers/CentrifugeController.cs
@@ -48,60 +48,75 @@ namespace CMS_Web.Controllers
         }
 
         [HttpPost]
-        public JsonResult UpdateStatusSim(SimCentrifugoModels model)
+        public JsonResult UpdateStatusSim(List<SimCentrifugoModels> model)
         {
             CMSSimsFactory _CMSSimsFactory = new CMSSimsFactory();
             int status = 0;
             bool updateResult = false;
-            if (!string.IsNullOrEmpty(model.name))
-            {
-                if (!string.IsNullOrEmpty(model.status))
-                {
-                    switch (model.status.Trim().ToLower())
-                    {
-                        case "đang kết nối":
-                            status = (int)Commons.SimStatus.WaitingConnect;
-                            break;
-                        case "lỗi kết nối":
-                            status = (int)Commons.SimStatus.ConnectFail;
-                            break;
-                        default:
-                            status = (int)Commons.SimStatus.Connected;
-                            break;
-                    }
-                }
-                else
-                {
-                    if (!string.IsNullOrEmpty(model.error))
-                    {
-                        status = (int)Commons.SimStatus.ConnectFail;
-                    }
-                    else
-                    {
-                        if (!string.IsNullOrEmpty(model.operatorName))
-                        {
-                            status = (int)Commons.SimStatus.Connected;
-                        }
-                        else
-                        {
-                            status = (int)Commons.SimStatus.WaitingConnect;
-                        }
-                    }
-                }
-                
-                updateResult = _CMSSimsFactory.UpdateStatusSim(model.name, status, model.operatorName);
-            }
             Dictionary<string, string> result = new Dictionary<string, string>();
-            if (updateResult)
+            if (model == null)
             {
-                result.Add("StatusCode", "200");
-                result.Add("Message", "Success");
+                result.Add("StatusCode", "500");
+                result.Add("Message", "Result is empty!");
             }
             else
             {
-                result.Add("StatusCode", "500");
-                result.Add("Message", "Server update Error");
+
+                foreach (SimCentrifugoModels item in model)
+                {
+                    if (!string.IsNullOrEmpty(item.name))
+                    {
+                        if (!string.IsNullOrEmpty(item.status))
+                        {
+                            switch (item.status.Trim().ToLower())
+                            {
+                                case "đang kết nối":
+                                    status = (int)Commons.SimStatus.WaitingConnect;
+                                    break;
+                                case "lỗi kết nối":
+                                    status = (int)Commons.SimStatus.ConnectFail;
+                                    break;
+                                default:
+                                    status = (int)Commons.SimStatus.Connected;
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            if (!string.IsNullOrEmpty(item.error))
+                            {
+                                status = (int)Commons.SimStatus.ConnectFail;
+                            }
+                            else
+                            {
+                                if (!string.IsNullOrEmpty(item.operatorName))
+                                {
+                                    status = (int)Commons.SimStatus.Connected;
+                                }
+                                else
+                                {
+                                    status = (int)Commons.SimStatus.WaitingConnect;
+                                }
+                            }
+                        }
+
+                        updateResult = _CMSSimsFactory.UpdateStatusSim(item.name, status, item.operatorName);
+                    }
+                }
+
+                
+                if (updateResult)
+                {
+                    result.Add("StatusCode", "200");
+                    result.Add("Message", "Success");
+                }
+                else
+                {
+                    result.Add("StatusCode", "500");
+                    result.Add("Message", "Server update Error");
+                }
             }
+            
             return Json(result, JsonRequestBehavior.AllowGet);
         }
         [HttpPost]
