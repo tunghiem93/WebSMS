bfac83e7e51071a4cdf2e4786d145dcabfd27e39
DESKTOP-GRKQD38\ThanhPham <thanh.pham@dki-sd.com>
2019-04-11 17:22:38 +0700

    up load

---
+++ CMS-Web/signalr/SynDataHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CMS_DTO.CMSSims;
+using Microsoft.AspNet.SignalR;
+
+namespace CMS_Web.signalr
+{
+    public class SynDataHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello("Xin Chao");
+        }
+
+        public static void SycnDataSim(List<CMS_SimsModels> listData)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<SynDataHub>();
+            hubContext.Clients.All.sycnDataSim(listData);
+        }
+
+    }
+}
